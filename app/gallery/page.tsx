deb49d97d87a682dd3ec4db161591b5ed9acb09f
Abdullah Khetran <khetranabdullah@gmail.com>
2023-09-11 02:50:23 +0500

    moved button component to seperate file

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,8 +1,4 @@
-"use client"
-
-import { CldUploadButton } from "next-cloudinary";
-import { UploadResult } from "../page";
-import { Button } from "@/components/ui/button";
+import UploadButton from "./upload-button";
 
 export default function GalleryPage() {
     const uploadPresetId = process.env.NEXT_PUBLIC_UPLOAD_PRESET_ID;
@@ -11,19 +7,7 @@ export default function GalleryPage() {
         <section>
             <div className="flex justify-between">
                 <h1 className="text-4xl font-bold">Gallery</h1>
-                {/* <Button > */}
-                <Button asChild>
-                    <div className="flex gap-2">
-                        <span>icon</span>
-                        <CldUploadButton
-                            uploadPreset={uploadPresetId}
-                            // @ts-ignore
-                            onUpload={(result: UploadResult) => {
-                                //   setImageId(result.info.public_id);
-                            }}
-                        />
-                    </div>
-                </Button>
+                <UploadButton />
             </div>
         </section>
     )
