52f6acfe1e8e560ba94db43d8823567384c39d34
Abdullah Khetran <khetranabdullah@gmail.com>
2023-09-11 04:10:33 +0500

    added mark as favorite functionality

--- app/gallery/cloudinary-image.tsx
+++ app/gallery/cloudinary-image.tsx
@@ -1,17 +1,32 @@
 "use client"
+import Heart from "@/components/icons/heart"
 import { CldImage } from "next-cloudinary"
+import cloudinary from "cloudinary"
+import { setAsFavorite } from "./actions"
+import { useTransition } from "react"
 
 type Props = {
     publicId: string
 }
 export default function CloudinaryImage({ publicId }: Props) {
+    const [transition, startTransition] = useTransition()
+
     return (
-        <CldImage
-            width="400"
-            height="300"
-            src={publicId}
-            sizes="100vw"
-            alt="Description of my image"
-        />
+        <div className="relative">
+            <CldImage
+                width="400"
+                height="300"
+                src={publicId}
+                sizes="100vw"
+                alt="Description of my image"
+            />
+            <Heart
+                onClick={() => {
+                    startTransition(() => {
+                        setAsFavorite(publicId)
+                    })
+                }}
+                className="absolute top-2 right-2 hover:text-red-500 cursor-pointer " />
+        </div>
     )
 }
