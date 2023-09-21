43d3f151928602753170f0140114fa5a33f012a4
Abdullah Khetran <khetranabdullah@gmail.com>
2023-09-22 04:45:29 +0500

    fixed folders nesting issue

--- components/actions.ts
+++ components/actions.ts
@@ -5,9 +5,16 @@ import cloudinary from "cloudinary"
 export async function addImageToAlbum(imageId: string, album: string) {
     await cloudinary.v2.api.create_folder(album);
 
+    // to remove any folder in the path and add image to folder at the root
+    let parts = imageId.split("/")
+    if (parts.length > 1) {
+        parts = parts.slice(1)
+    }
+    const imagePublicId = parts.join("/")
+
     await cloudinary.v2.uploader.rename(
         imageId,
-        `${album}/${imageId}`
+        `${album}/${imagePublicId}`
     )
 
 }
