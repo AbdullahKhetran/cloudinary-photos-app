4a5cc0824b1de785545fa224945b67d6854bf679
Abdullah Khetran <khetranabdullah@gmail.com>
2023-09-26 21:40:48 +0500

    added generative-fill using ai option

--- app/edit/page.tsx
+++ app/edit/page.tsx
@@ -1,3 +1,8 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { CldImage } from "next-cloudinary"
+import { useState } from "react"
 
 // got this by console logging props
 type Props = {
@@ -8,8 +13,37 @@ type Props = {
 
 export default function EditPage({ searchParams }: Props) {
     const publicId = searchParams.publicId
+    const [transformation, setTransformation] = useState<undefined | "generative-fill">()
 
     return (
-        <div>Edit Page</div>
+        <section>
+            <div className="flex flex-col gap-8">
+                <div className="flex justify-between">
+                    <h1 className="text-4xl font-bold">Edit {publicId}</h1>
+                </div>
+
+                <Button onClick={() => setTransformation("generative-fill")}>Generative Fill</Button>
+
+                <div className="grid grid-cols-2 gap-12">
+                    <CldImage
+                        src={publicId}
+                        alt="Edit your image"
+                        width={300}
+                        height={200}
+                    />
+
+                    {transformation === "generative-fill" &&
+                        <CldImage
+                            src={publicId}
+                            alt="Edit your image"
+                            width={300}
+                            height={200}
+                            crop="pad" // Returns the given size with padding
+                            fillBackground // Uses AI to extend image
+                        />
+                    }
+                </div>
+            </div>
+        </section>
     )
 }
